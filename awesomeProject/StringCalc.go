0e2dd47626da907ad1d144178e2c3366726a2df6
Milenim <148369959+Milenim@users.noreply.github.com>
2024-08-16 18:04:35 +0800

    Add files via upload

--- awesomeProject/StringCalc.go
+++ awesomeProject/StringCalc.go
@@ -9,55 +9,83 @@ import (
 )
 
 func main() {
-	var userInput []string
-	seps := []string{"+", "/", "-", "*"}
-
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Введите выражение: ")
 	text, _ := reader.ReadString('\n')
 	text = strings.TrimSpace(text)
 
-	var sep string
-	for _, sep = range seps {
-		if strings.Contains(text, sep) {
-			userInput = strings.Split(text, sep)
-			break
-		}
+	sep, startIndex := findOperatorOutsideQuotes(text)
+	if sep == "" {
+		panic("Неверный оператор операции")
 	}
 
-	str1 := strings.Trim(userInput[0], "\" ")
+	userInput := []string{
+		strings.TrimSpace(text[:startIndex]),
+		strings.TrimSpace(text[startIndex+1:]),
+	}
+
+	str1 := strings.TrimSpace(strings.Trim(userInput[0], "\""))
 	secondOperand := strings.TrimSpace(userInput[1])
+
+	if len(str1) > 10 {
+		panic("Длина строки больше 10 символов")
+	}
+
+	var result string
 
 	switch sep {
 	case "+":
 		str2 := strings.Trim(secondOperand, "\"")
-		result := str1 + str2
-		fmt.Printf("\"%s\"\n", result)
+		result = str1 + str2
 	case "*":
 		parseInt, err := strconv.Atoi(secondOperand)
 		if err != nil || parseInt < 1 || parseInt > 10 {
 			panic("Неверный множитель: ожидается целое число от 1 до 10")
 		}
-		result := strings.Repeat(str1, parseInt)
-		fmt.Printf("\"%s\"\n", result)
+		result = strings.Repeat(str1, parseInt)
 	case "/":
 		parseInt, err := strconv.Atoi(secondOperand)
 		if err != nil || parseInt < 1 || parseInt > 10 {
-			panic("Неверный делитель: ожидается целое число от 1 до 10.")
+			panic("Неверный делитель: ожидается целое число от 1 до 10")
 		}
 		if len(str1) < parseInt {
 			panic("Длина строки короче делителя")
 		}
-		result := str1[:len(str1)/parseInt]
-		fmt.Printf("\"%s\"\n", result)
+		result = str1[:len(str1)/parseInt]
 	case "-":
 		str2 := strings.Trim(secondOperand, "\"")
 		if !strings.Contains(str1, str2) {
 			panic("Вычитаемое не найдено в строке")
 		}
-		result := strings.Replace(str1, str2, "", 1)
-		fmt.Printf("\"%s\"\n", result)
+		result = strings.Replace(str1, str2, "", 1)
 	default:
 		panic("Неподдерживаемая операция")
 	}
+
+	if len(result) > 40 {
+		result = result[:40] + "..."
+	}
+
+	fmt.Printf("\"%s\"\n", result)
+}
+
+func findOperatorOutsideQuotes(text string) (string, int) {
+	seps := []string{"+", "/", "-", "*"}
+	inQuotes := false
+
+	for i, char := range text {
+		switch char {
+		case '"':
+			inQuotes = !inQuotes
+		default:
+			if !inQuotes {
+				for _, sep := range seps {
+					if string(char) == sep {
+						return sep, i
+					}
+				}
+			}
+		}
+	}
+	return "", -1
 }
